44afe6ad0940a757cc837a76bb650b4b461fe7e4
charlesbio <charles.bio@havocdigital.com>
2013-07-09 18:13:46 +0800

    7th commit

--- web.js
+++ web.js
@@ -1,19 +1,11 @@
-var http = require('http'),
-fs = require('fs');
+var http = require('http');
+var fs = require('fs');
+var sys = require('sys');
 
-var app = express.createServer(express.logger());
-
-fs.readFile('index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }      
-        http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(function(req, res){
+    fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(8000);
