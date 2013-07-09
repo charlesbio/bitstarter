a60eac52b65fd3438a0eca380f059f2ab813dea3
charlesbio <charles.bio@havocdigital.com>
2013-07-09 18:11:44 +0800

    6th commit

--- web.js
+++ web.js
@@ -3,10 +3,15 @@ fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-fs.readFile('./index.html', function (err, html) {
+fs.readFile('index.html', function (err, html) {
     if (err) {
         throw err; 
     }      
+        http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
