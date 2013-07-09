7b75ed3576dceefb83b30db4d6ba1210e506480f
charlesbio <charles.bio@havocdigital.com>
2013-07-09 18:08:32 +0800

    5th commit

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
-var express = require('express');
+var http = require('http'),
+fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send("index.html");
-
-});
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }      
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
